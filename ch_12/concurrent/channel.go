017dae75f3a9c1089be17ed751b8759857a1b562
ankur <ankurdwivedi.dce@gmail.com>
2026-04-08 18:28:54 +0530

    ch_12:updated

--- ch_12/concurrent/channel.go
+++ ch_12/concurrent/channel.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
-	msgChan := make(chan string)
-	go sendMessage(2, msgChan)
-	go sendMessage(3, msgChan)
+	myChan := make(chan int, 1)
+	defer close(myChan)
+	wg := &sync.WaitGroup{}
 
-	msg1 := <-msgChan
-	fmt.Println(msg1)
+	//myChan <- 5
+	//fmt.Print(<-myChan) // will deadlock cause no go routine to listen
 
-	msg2 := <-msgChan
-	fmt.Println(msg2)
+	// makes this send only
+	wg.Add(2)
+	go func(ch chan<- int, wg *sync.WaitGroup) {
+		ch <- 5
+		wg.Done()
+	}(myChan, wg)
 
-	close(msgChan)
+	// makes this receive only
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		val, isOpen := <-ch
+		if isOpen {
+			fmt.Print(val)
+		}
+		wg.Done()
+		//close(ch)   // stops you from closing the channel
+	}(myChan, wg)
 
-}
-
-func sendMessage(num int, msgChan chan<- string) {
-	fmt.Printf("sending message: %d\n\n", num)
-	time.Sleep(time.Second * time.Duration(num)) // simulate some work
-
-	msg := fmt.Sprintf("Message %d! sent", num)
-	msgChan <- msg
+	wg.Wait()
 }
